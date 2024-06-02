71a468c7cbffb2a062801ae2e21eab35bb5ca7a8
Mohamed Jasim <84412131+MohamedJasim20@users.noreply.github.com>
2024-06-02 19:49:22 +0530

    first commit

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Position { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+    }
+}
